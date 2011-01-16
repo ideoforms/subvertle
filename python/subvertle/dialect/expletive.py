e22ee286c0de28d1fc912a5850647acdaf6af55e
Daniel Jones <d.jones@gold.ac.uk>
2011-01-16 11:23:31 +0000

    fixed dialect

--- python/subvertle/dialect/expletive.py
+++ python/subvertle/dialect/expletive.py
@@ -1,12 +1,13 @@
 import random
+from dialectp import *
 
-class expletive (dialect):
+class expletive (dialectp):
 	swears=[]
 	alpha = 1
 
-    def __init__(self):
-        self.tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+|[^\w\s]+')
-        self.tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
+	def __init__(self):
+		self.tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+|[^\w\s]+')
+		self.tagger = nltk.UnigramTagger(nltk.corpus.brown.tagged_sents())
 		makeSwear()
 
 	def makeSwear():
@@ -18,20 +19,20 @@ class expletive (dialect):
 		index = random.paretodistribution(alpha)*len(swears)
 		return swears[index]
 
-    def process(self, text):
-        tokenized = self.tokenizer.tokenize(text)
-        tagged = self.tagger.tag(tokenized)
+	def process(self, text):
+		tokenized = self.tokenizer.tokenize(text)
+		tagged = self.tagger.tag(tokenized)
 
-        done = {}
-        for word, type in tagged:
-            if word in done:
-                continue
-            # # # print "%s: %s" % (type, word)
-            if type and type[0] == 'N' and random.random() < 0.4:
+		done = {}
+		for word, type in tagged:
+			if word in done:
+				continue
+			# # # print "%s: %s" % (type, word)
+			if type and type[0] == 'N' and random.random() < 0.4:
 				swear = makeSwear()
-                text = re.sub(r"\b%s\b" % word, "%s %s" % swear, word, text)
-                done[word] = 1
+				text = re.sub(r"\b%s\b" % word, "%s %s" % swear, word, text)
+				done[word] = 1
 
-        return text
+		return text
 
 
